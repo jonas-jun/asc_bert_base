b01a03cba8ea0069439f963b8515c3e57e491e34
jonas-Jun <junhot08@gmail.com>
2021-05-20 20:38:54 +0900

    add main.ipynb, and Analysis.ipynb

--- models/bert_attscores.py
+++ models/bert_attscores.py
@@ -192,6 +192,69 @@ class Bert_AttScore_RNN_add(nn.Module):
         output = self.fc1(output)
         return output, top_k_words
 
+class Bert_AttScore_forCLS_RNN(nn.Module):
+    def __init__(self, opt, embed_dim=768, rnn_hid_dim=256, fc_hid_dim=128, bidirectional=True, top_k=3,
+            att_head='all', att_pooling='gru'):
+        super(Bert_AttScore_forCLS_RNN, self).__init__()
+        self.num_classes = opt.num_classes
+        self.embed_dim = embed_dim
+        self.rnn_hid_dim = rnn_hid_dim
+        self.fc_hid_dim = fc_hid_dim
+        self.bidirectional = bidirectional
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.rnn_pool = True if att_pooling in ['rnn', 'lstm', 'gru'] else False
+        if self.rnn_pool:
+            self.rnn = nn.GRU(input_size=768, hidden_size=rnn_hid_dim, num_layers=1, bidirectional=self.bidirectional, 
+                batch_first=True)
+        self.top_k = top_k
+        self.att_head = att_head
+        self.att_pooling = att_pooling
+        if self.att_pooling == 'concat':
+            self.fc1 = nn.Linear((self.embed_dim) * top_k, self.num_classes)
+        elif self.att_pooling in ['rnn', 'lstm', 'gru']:
+            self.fc1 = nn.Linear(self.rnn_hid_dim*(self.bidirectional+1), self.num_classes)
+        else:
+            self.fc1 = nn.Linear(self.embed_dim, self.num_classes)
+        self.device = opt.device
+
+    def forward(self, input_ids, att_mask, token_ids):
+        # <SEP> ids
+        sep_ids = [(i==102).nonzero(as_tuple=True)[0] for i in input_ids]
+        # aspect words ids
+        asp_ids = [list(range(s[0]+1, s[-1])) for s in sep_ids]
+        
+        output_dict = self.bert(input_ids=input_ids, attention_mask=att_mask, token_type_ids=token_ids,
+            output_attentions=True, encoder_hidden_states=True, return_dict=True)
+
+        # get top-k att idx in final att layer
+        last_att = output_dict.attentions[-1]
+        target_heads = last_att if self.att_head == 'all' else last_att[:, self.att_head, :, :]
+        atts = [torch.mean(i, dim=0) for i in target_heads] # len(atts): batch_size, atts[0].shape: (200, 200)
+        
+        # get top-k score words ids
+        top_k_words = list() # len 16 list, [0]: len top-k list [14, 3, 17]
+        for att, asp, sep in zip(atts, asp_ids, sep_ids):
+            att_score = att[0, :] # (200), sum att scores of asp words
+            top_k_idx = torch.sort(att_score[1:sep[0]], descending=True).indices[:self.top_k] # (3), exclude [CLS], [SEP], aspect words
+            if self.rnn_pool:
+                while len(top_k_idx) < self.top_k:
+                    top_k_idx = top_k_idx.tolist()
+                    top_k_idx += [top_k_idx[0]]
+                    top_k_idx = torch.tensor(top_k_idx) # append first token if length of sentence is less than top_k
+                top_k_idx = torch.sort(top_k_idx, descending=False).values # order
+            top_k_words.append(top_k_idx+1) # consider [CLS] token for idx
+        
+        # get top-k hidden states
+        hids = output_dict.last_hidden_state
+        output = get_hiddens(last_hids=hids, top_k_list=top_k_words, pooling=self.att_pooling) # only top-k att score words
+        
+        # gru에 집어 넣는데.. (16, 3, 768)이 들어가서 (16, 3, 256)이 나온다. 여기서 [:, -1, :]만 사용할 것 (16, 256)
+        output, _ = self.rnn(output)
+        output = output[:, -1, :] # last output
+        
+        output = self.fc1(output)
+        return output, top_k_words
+
 def get_hiddens(last_hids, top_k_list, pooling='mean'):
     '''
     @args
